356542f21017b8c7b5d318a8f3783f617d15b15e
mikephp <imlypb@gmail.com>
2015-07-16 23:13:40 +0800

    Update download_zzdt.py

--- src/python/download_zzdt.py
+++ src/python/download_zzdt.py
@@ -5,7 +5,7 @@ i=1
 while True:
     url = "http://weibo.com/zhengzhouditie?is_search=0&visible=0&is_tag="\
     "0&profile_ftype=1&page="+str(i)
-    print "当前时间 " +time.strftime("%c")
+    print "鏃堕棿 " +time.strftime("%c")
     print url
     output = open('zzdt.html','w+')
     webdata = urlopen(url).read()

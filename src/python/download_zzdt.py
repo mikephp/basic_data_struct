6c3da606d5352bc7635853becf914bb3c63b9138
mikephp <imlypb@google.com>
2015-07-16 23:12:32 +0800

    add python script

---
+++ src/python/download_zzdt.py
@@ -0,0 +1,15 @@
+# coding: GBK
+from urllib import urlopen
+import time
+i=1
+while True:
+    url = "http://weibo.com/zhengzhouditie?is_search=0&visible=0&is_tag="\
+    "0&profile_ftype=1&page="+str(i)
+    print "当前时间 " +time.strftime("%c")
+    print url
+    output = open('zzdt.html','w+')
+    webdata = urlopen(url).read()
+    time.sleep(2)
+    output.write(webdata)
+    i = i+1
+output.close()

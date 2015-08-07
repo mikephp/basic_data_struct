fe7221a2a3f89cf05bc50040f72828d443ebaae4
mikephp <imlypb@gmail.com>
2015-08-07 10:02:58 +0800

    add scan_ip_port.py demo

---
+++ src/python/scan_ip_port.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: gbk -*-
+import socket,time
+def scan_ip_port():
+    file_obj = open('ip.txt')
+      for line in file_obj:
+        try:
+          sc=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+          ip = line.split()[0]
+          port = int(line.split()[1])
+          print ip,port
+          #设置超时时间（0.0）
+          sc.settimeout(2)
+          sc.connect((ip,port))
+          timenow=time.localtime()
+          datenow = time.strftime('%Y-%m-%d %H:%M:%S', timenow)
+          logstr="%s:%s 连接成功->%s \n" %(ip,port,datenow)
+          print logstr
+          sc.close()
+        except:
+          file = open("log.txt", "a")
+          timenow=time.localtime()
+          datenow = time.strftime('%Y-%m-%d %H:%M:%S', timenow)
+          logstr="%s:%s 连接失败->%s \n" %(ip,port,datenow)
+          print logstr
+          file.write(logstr)
+          file.close()
+      file_obj.close()
+  
+scan_ip_port()
+print "sleep 10....."
+time.sleep(10)
+print "task over"

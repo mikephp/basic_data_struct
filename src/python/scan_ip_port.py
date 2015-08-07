4e67c2743be4cd15e5217b373c0829dae78dabab
mikephp <imlypb@gmail.com>
2015-08-07 10:14:26 +0800

    add scan_ip_port.py demo

--- src/python/scan_ip_port.py
+++ src/python/scan_ip_port.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: gbk -*-
-import socket,time
+import socket,time,sys,os
+current_file_path = sys.path[0]
+print current_file_path
 def scan_ip_port():
-    file_obj = open('ip.txt')
-      for line in file_obj:
-        try:
-          sc=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-          ip = line.split()[0]
-          port = int(line.split()[1])
-          print ip,port
-          #设置超时时间（0.0）
-          sc.settimeout(2)
-          sc.connect((ip,port))
-          timenow=time.localtime()
-          datenow = time.strftime('%Y-%m-%d %H:%M:%S', timenow)
-          logstr="%s:%s 连接成功->%s \n" %(ip,port,datenow)
-          print logstr
-          sc.close()
-        except:
-          file = open("log.txt", "a")
-          timenow=time.localtime()
-          datenow = time.strftime('%Y-%m-%d %H:%M:%S', timenow)
-          logstr="%s:%s 连接失败->%s \n" %(ip,port,datenow)
-          print logstr
-          file.write(logstr)
-          file.close()
-      file_obj.close()
+	file_obj = open(current_file_path+'/ip.txt')
+	file = open(current_file_path+"/log.txt", "a")
+	for line in file_obj:
+		try:
+			sc=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+			ip = line.split()[0]
+			port = int(line.split()[1])
+			print ip,port
+			#设置超时时间（0.0）
+			sc.settimeout(2)
+			sc.connect((ip,port))
+			timenow=time.localtime()
+			datenow = time.strftime('%Y-%m-%d %H:%M:%S', timenow)
+			logstr="%s:%s 连接成功->%s \n" %(ip,port,datenow)
+			print logstr
+			sc.close()
+		except:
+			timenow=time.localtime()
+			datenow = time.strftime('%Y-%m-%d %H:%M:%S', timenow)
+			logstr="%s:%s 连接失败->%s \n" %(ip,port,datenow)
+			print logstr
+			file.write(logstr)
+	file_obj.close()
+	file.close()
   
 scan_ip_port()
 print "sleep 10....."
